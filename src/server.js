7ca5f3cf9bb05f163fe8438bc01b5c2b20c82664
MartinWeigl125 <martinweigljun@gmail.com>
2025-10-30 13:36:03 +0100

    feat(user-signup-login): basic signup and login for users in json db with tests (v0.1.0)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import Hapi from "@hapi/hapi";
+import Inert from "@hapi/inert";
+import Vision from "@hapi/vision";
+import Handlebars from "handlebars";
 import path from "path";
-
 import { fileURLToPath } from "url";
+import { webRoutes } from "./web-routes.js";
+import { db } from "./models/db.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,6 +14,21 @@ async function init() {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
   });
+  await server.register(Vision);
+  await server.register(Inert);
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "./views",
+    layoutPath: "./views/layouts",
+    partialsPath: "./views/partials",
+    layout: true,
+    isCached: false,
+  });
+  db.init();
+  server.route(webRoutes);
   await server.start();
   console.log("Server running on %s", server.info.uri);
 }
